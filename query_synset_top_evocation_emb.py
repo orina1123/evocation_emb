618f1cbfb22bec1bc793aef1615ad4e1c1f53f13
Orina Shiue <orina1123@gmail.com>
2020-07-25 10:13:44 +0800

    top synset query, show words in synset

--- query_synset_top_evocation_emb.py
+++ query_synset_top_evocation_emb.py
@@ -2,11 +2,14 @@ import argparse
 import gensim
 import numpy as np
 
+from nltk.corpus import wordnet as wn
+
 # settings
 ap = argparse.ArgumentParser()
 
 ap.add_argument("pair_score_file", type=argparse.FileType('r'), help="TSV of synset pair - score [controlled.standard.synset_pair_avg_score.tsv]")
-ap.add_argument("emb_txt_prefix", type=str, help="path prefix to trained out/in synset embeddings/models, *.[out|in].vec.txt / *.")
+ap.add_argument("emb_txt_prefix", type=str, help="path prefix to trained out/in synset embeddings, *.[out|in].vec.txt")
+ap.add_argument("-n", type=int, default=30, help="show top n synsets")
 
 args = ap.parse_args()
 
@@ -20,8 +23,6 @@ for line in args.pair_score_file:
 # load embeddings
 out_emb = gensim.models.KeyedVectors.load_word2vec_format(args.emb_txt_prefix + ".out.vec.txt", binary=False)
 in_emb = gensim.models.KeyedVectors.load_word2vec_format(args.emb_txt_prefix + ".in.vec.txt", binary=False)
-# load keras model
-#TODO
 
 # accept queries
 while True:
@@ -31,18 +32,37 @@ while True:
     if s not in out_emb:
         print("! synset %s not found in vocab" % (s))
         continue
-    
-    print("--[ forward evocation ]-->")
-    synset_scores_list = [] # (synset t, human_score, dot(out_emb[s], in_emb[t]), model_pred_score)
+    print(s, [str(lemma.name()) for lemma in wn.synset(s).lemmas()])    
+    forward_synset_scores_list = [] # (synset t, human_score, dot(out_emb[s], in_emb[t]), model_score)
+    backward_synset_scores_list = []
     for t in in_emb.vocab:
         if t == s:
             continue
+        #forward
         human_score = None
         if (s, t) in pair_score:
             human_score = pair_score[(s, t)]
         dot_score = np.dot(out_emb[s], in_emb[t])
-        synset_scores_list.append( (t, human_score, dot_score) )
-    for (t, human_score, dot_score) in sorted(synset_scores_list, key=lambda p: p[-1], reverse=True)[:20]:
-        print("", t, "\t[human]:", human_score, "\t[out*in]:", dot_score)
+        forward_synset_scores_list.append( (t, human_score, dot_score) )        
+        #backward
+        human_score = None
+        if (t, s) in pair_score:
+            human_score = pair_score[(t, s)]
+        dot_score = np.dot(out_emb[t], in_emb[s])
+        backward_synset_scores_list.append( (t, human_score, dot_score) )
+
+    print("="*50)
+    print(" --[ forward evocation ]-->")
+    print("="*50)
+    for (t, human_score, dot_score) in sorted(forward_synset_scores_list, key=lambda p: p[-1], reverse=True)[:args.n]:
+        print(s, "-->", t, "\t\t[human]:", human_score, "\t[out*in]:", dot_score)
+        print(" "*(len(s)+4), [str(lemma.name()) for lemma in wn.synset(t).lemmas()])
+    print("="*50)
+    print(" <--[ backward evocation ]--")
+    print("="*50)
+    for (t, human_score, dot_score) in sorted(backward_synset_scores_list, key=lambda p: p[-1], reverse=True)[:args.n]:
+        print(s, "<--", t, "\t\t[human]:", human_score, "\t[out*in]:", dot_score)
+        print(" "*(len(s)+4), [str(lemma.name()) for lemma in wn.synset(t).lemmas()])
+
 
     print("")
