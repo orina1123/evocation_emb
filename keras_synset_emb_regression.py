95c01e97d434eda54aebc0900b1a9e1752132d7c
Orina Shiue <orina1123@gmail.com>
2020-08-01 18:09:36 +0800

    add retrofitting regularization term

--- keras_synset_emb_regression.py
+++ keras_synset_emb_regression.py
@@ -5,6 +5,7 @@ np.random.seed(710)
 
 from keras.models import Sequential, Model
 from keras.layers import Dense, Embedding, Reshape, Activation, Input
+from keras.layers import Subtract, Lambda
 from keras.layers.merge import Dot
 import tensorflow as tf
 from tensorflow.keras.constraints import unit_norm, max_norm, non_neg
@@ -111,6 +112,11 @@ if args.init_out is not None:
     pre_emb = gensim.models.KeyedVectors.load_word2vec_format(args.init_out, binary=False)
     out_emb_W = [ build_emb_mat_from_gensim(id2syn, args.emb_size, pre_emb, init_emb_mat=out_emb.get_weights()[0]) ]
     out_emb.set_weights(out_emb_W)
+    if args.retro_reg > 0.0:
+        out_emb_reg = Embedding(V, args.emb_size, name="out_emb_reg", trainable=False, weights=out_emb_W)
+        #out_emb_reg.set_weights(out_emb_W)
+        out_embbed_reg = out_emb_reg(out_inputs)
+
 
 #  emb_in
 in_inputs = Input(name="in_syn", shape=(1, ), dtype='int32')
@@ -122,7 +128,12 @@ if args.init_in is not None:
     if args.init_in != args.init_out:
         pre_emb = gensim.models.KeyedVectors.load_word2vec_format(args.init_in, binary=False)
     in_emb_W = [ build_emb_mat_from_gensim(id2syn, args.emb_size, pre_emb, init_emb_mat=in_emb.get_weights()[0]) ]
-    in_emb.set_weights(in_emb_W)
+    in_emb.set_weights(in_emb_W) 
+    if args.retro_reg > 0.0:
+        in_emb_reg = Embedding(V, args.emb_size, name="in_emb_reg", trainable=False, weights=in_emb_W)
+        #in_emb_reg.set_weights(in_emb_W)
+        in_embbed_reg = out_emb_reg(in_inputs)
+
 
 #  dot(out_emb, in_emb)
 o = Dot(axes=2)([out_embbed, in_embbed])
@@ -132,10 +143,19 @@ if args.multiply_layer:
 if args.sigmoid:
     o = Activation('sigmoid')(o)
 
-model = Model(inputs=[out_inputs, in_inputs], outputs=o)
+if args.retro_reg > 0.0:
+    out_emb_err = Subtract()([out_embbed, out_embbed_reg])
+    #out_emb_err = Lambda(lambda x: x**2)(out_emb_err)
+    in_emb_err = Subtract()([in_embbed, in_embbed_reg])
+    model = Model(inputs=[out_inputs, in_inputs], outputs=[o, out_emb_err, in_emb_err])
+else:
+    model = Model(inputs=[out_inputs, in_inputs], outputs=o)
 model.summary()
 optm = Adam(lr=args.learning_rate)
-model.compile(loss='mse', optimizer=optm, metrics=["mse", "mae"])
+if args.retro_reg > 0.0:
+    model.compile(loss=["mse", "mse", "mse"], loss_weights=[1.0, args.retro_reg, args.retro_reg], optimizer=optm, metrics=["mse"])
+else:
+    model.compile(loss='mse', optimizer=optm, metrics=["mse", "mae"])
 
 # train model
 cb = []
@@ -146,7 +166,11 @@ else:
 if args.val_split > 0.0:
     saveBestModel = ModelCheckpoint(best_ckpt_path, monitor='val_mse', verbose=0, save_best_only=True, mode='min')
     cb.append(saveBestModel)
-model.fit(x=[X_out_train, X_in_train], y=Y_train, epochs=args.epochs, batch_size=32, validation_split=args.val_split, callbacks=cb)
+if args.retro_reg > 0.0:
+    _Y_reg = np.zeros( (Y_train.shape[0], args.emb_size) )
+    model.fit(x=[X_out_train, X_in_train], y=[Y_train, _Y_reg, _Y_reg], epochs=args.epochs, batch_size=32, validation_split=args.val_split, callbacks=cb)
+else:
+    model.fit(x=[X_out_train, X_in_train], y=Y_train, epochs=args.epochs, batch_size=32, validation_split=args.val_split, callbacks=cb)
 
 # go to best checkpoint
 if args.val_split > 0.0:
@@ -156,7 +180,11 @@ else:
 
 # evaluate model
 if args.test_split > 0.0:
-    loss, mse, mae = model.evaluate([X_out_test, X_in_test], Y_test)
+    if args.retro_reg > 0.0:
+        _Y_reg = np.zeros( (Y_test.shape[0], args.emb_size) )
+        loss, mse, mae = model.evaluate([X_out_test, X_in_test], [Y_test, _Y_reg, _Y_reg])
+    else:
+        loss, mse, mae = model.evaluate([X_out_test, X_in_test], Y_test)
     print("Test loss: %.4f, MSE: %.4f, MAE: %.4f" % (loss, mse, mae))
 
 # save embeddings
